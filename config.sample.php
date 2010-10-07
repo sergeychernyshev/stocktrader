7e89c67d2f68376770ff2e85ccdddcae7175fb93
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-10-06 23:38:08 -0400

    Added basic UserBase setup

---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+# set db credentials
+$db = new mysqli( 'localhost', 'stocktrader', '..........', 'stocktrader');
+
+# put some random string here
+$sessionSecret = '.....................';
+
---
+++ users_config.php
@@ -0,0 +1,21 @@
+<?php
+require_once(dirname(__FILE__).'/global.php');
+UserConfig::setDB($db);
+
+if ($facebookAPIKey) {
+	require_once(dirname(__FILE__).'/users/modules/facebook/index.php');
+	UserConfig::$modules[] = new FacebookAuthenticationModule($facebookAPIKey, $facebookSecret);
+}
+
+if ($googleFriendConnectSiteID) {
+	require_once(dirname(__FILE__).'/users/modules/google/index.php');
+	UserConfig::$modules[] = new GoogleAuthenticationModule($googleFriendConnectSiteID);
+}
+
+require_once(dirname(__FILE__).'/users/modules/usernamepass/index.php');
+UserConfig::$modules[] = new UsernamePasswordAuthenticationModule();
+
+UserConfig::$SESSION_SECRET = $sessionSecret;
+
+#UserConfig::$header = dirname(__FILE__).'/header.php';
+#UserConfig::$footer = dirname(__FILE__).'/footer.php';
