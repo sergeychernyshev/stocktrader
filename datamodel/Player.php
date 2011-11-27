2f04173fb5acda015e21c215865fe0daabae7250
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-11-27 18:40:19 -0400

    Connected player to it's DB representation

--- datamodel/Player.php
+++ datamodel/Player.php
@@ -13,9 +13,13 @@ class Player
 {
 	private $user;
 
-	public function __construct($user)
+	// if null, it's a dummy user with no DB representation
+	private $player_id;
+
+	public function __construct($user, $player_id = null)
 	{
 		$this->user = $user;
+		$this->player_id = $player_id;
 	}
 
 	public function asString()
@@ -35,5 +39,36 @@ class Player
 
 	public function getUser() {
 		return $this->user;
+	}
+
+	public static function getPlayer($user) {
+		$db = UserConfig::getDB();
+
+		$daily_activity = array();
+
+		if ($stmt = $db->prepare('SELECT player_id FROM player WHERE user_id = ?'))
+		{
+			if (!$stmt->bind_param('i', $user->getID()))
+			{
+				 throw new Exception("Can't bind parameter".$stmt->error);
+			}
+			if (!$stmt->execute())
+			{
+				throw new Exception("Can't execute statement: ".$stmt->error);
+			}
+			if (!$stmt->bind_result($player_id))
+			{
+				throw new Exception("Can't bind result: ".$stmt->error);
+			}
+
+			$stmt->fetch();
+			$stmt->close();
+		}
+		else
+		{
+			throw new Exception("Can't prepare statement: ".$db->error);
+		}
+
+		return is_null($player_id) ? null : new Player($user, $player_id);
 	}
 }
